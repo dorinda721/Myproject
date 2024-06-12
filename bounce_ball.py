7acee1259437201c1cda4b1d4c5eff2dd7fd9457
dorinda721 <ddlove721@gmail.com>
2024-06-12 17:56:01 +0800

    Update bounce_ball.py

--- bounce_ball.py
+++ bounce_ball.py
@@ -39,10 +39,46 @@ def bounce(event):
             y = GRAVITY + y
             ball.move(VX, y)  # move為會執行的距離
             pause(DELAY)
-            if ball.y >= window.height:  # 當超出範圍的時候即反彈
+            if ball.y + ball.height >= window.height and y > 0:  # 當超出範圍的時候即反彈
+                ball.y = window.height - ball.height
                 y = -y*REDUCE
         window.add(ball, START_X, START_Y)
         button = False
+
+#2
+
+
+def main():
+    """
+    This program simulates a bouncing ball at (START_X, START_Y)
+    that has VX as x velocity and 0 as y velocity. Each bounce reduces
+    y velocity to REDUCE of itself.
+    """
+    global time, button
+    ball.filled = True
+    window.add(ball, START_X, START_Y)
+    onmouseclicked(bounce)
+    y = 0
+    while time < 3:  # 確認是否為有效點擊，以及次數是否已達三次
+        if button:
+            if ball.x < window.width:  # x不超過時就一直執行整個while
+                y = GRAVITY + y
+                ball.move(VX, y)  # move為會執行的距離
+                if ball.y + ball.height >= window.height and y > 0:  # 當超出範圍的時候即反彈
+                    ball.y = window.height - ball.height
+                    y = -y*REDUCE
+            else:
+                window.add(ball, START_X, START_Y)
+                button = False
+                time += 1
+        pause(DELAY)
+
+
+def bounce(event):
+    global button
+    if not button:
+        button = True
+
 
 
 if __name__ == "__main__":
